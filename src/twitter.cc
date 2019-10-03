7c08eff1eab6b6913a5ed5ebea692587b505f92b
Clement Magnard <magnar_c@epita.fr>
2019-10-03 13:57:26 +0200

    cmake: python bidings auto created

---
+++ src/twitter.cc
@@ -0,0 +1,6 @@
+#include "twitter.hh"
+
+void Twitter::postTweet(unsigned int userid, unsigned int tweetid) {}
+void Twitter::follow(unsigned int followee, unsigned int follower) {}
+void Twitter::unfollow(unsigned int followee, unsigned int follower) {}
+std::vector<unsigned int> Twitter::getNewsFeed(unsigned int followee) {}
--- src/twitter.hh
+++ src/twitter.hh
@@ -3,21 +3,19 @@
 #include <map>
 #include <vector>
 
-struct Twitt
-{
-    unsigned int id;
-    time_t timestamp;
+struct Twitt {
+  unsigned int id;
+  time_t timestamp;
 };
 
-class Twitter
-{
-  public:
-    void postTweet(unsigned int userid, unsigned int tweetid);
-    void follow(unsigned int followee, unsigned int follower);
-    void unfollow(unsigned int followee, unsigned int follower);
-    std::vector<Twitt> getNewsFeed(unsigned int followee);
+class Twitter {
+public:
+  void postTweet(unsigned int userid, unsigned int tweetid);
+  void follow(unsigned int followee, unsigned int follower);
+  void unfollow(unsigned int followee, unsigned int follower);
+  std::vector<unsigned int> getNewsFeed(unsigned int followee);
 
-  private:
-    std::map<unsigned int, std::vector<unsigned int>> users_;
-    std::map<unsigned int, std::vector<Twitt>> tweet_map_;
+private:
+  std::map<unsigned int, std::vector<unsigned int>> users_;
+  std::map<unsigned int, std::vector<Twitt>> tweet_map_;
 };
