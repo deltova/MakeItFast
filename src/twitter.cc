64eebbfeaf513f24d94656e7312c581c0be39fa7
Clement Magnard <magnar_c@epita.fr>
2019-10-03 14:33:33 +0200

    cmake: run test with make check

--- src/twitter.cc
+++ src/twitter.cc
@@ -1,6 +1,15 @@
 #include "twitter.hh"
 
-void Twitter::postTweet(unsigned int userid, unsigned int tweetid) {}
-void Twitter::follow(unsigned int followee, unsigned int follower) {}
-void Twitter::unfollow(unsigned int followee, unsigned int follower) {}
-std::vector<unsigned int> Twitter::getNewsFeed(unsigned int followee) {}
+void Twitter::postTweet(unsigned int userid, unsigned int tweetid)
+{
+}
+void Twitter::follow(unsigned int followee, unsigned int follower)
+{
+}
+void Twitter::unfollow(unsigned int followee, unsigned int follower)
+{
+}
+std::vector<unsigned int> Twitter::getNewsFeed(unsigned int followee)
+{
+    return std::vector<unsigned int>();
+}
